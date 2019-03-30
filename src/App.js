69fcc89a85c302250765bda8e3d29cb0e9d8e76b
simontollstern <simoon909@hotmail.com>
2019-03-30 21:31:33 +0100

    Modified some comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Dashboard from './components/dashboard/Dashboard';
 import './App.css';
 
+// The root of the site of which components are rendered in
 class App extends Component {
   render() {
     return (
--- src/components/user/User.js
+++ src/components/user/User.js
@@ -3,7 +3,7 @@ import style from './User.module.css';
 
 // The list items containing the user's names
 // This may be an overcomplicated solution, but I couldn't get it to work with
-// CSS Modules any other way
+// CSS Modules any other way (I suppose come conditional could be used)
 function User(props){
   if(props.colorToggled){
     return <li className={[style.user, style.green].join(' ')}>{props.name}</li>
